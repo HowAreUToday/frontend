a0383ba81c161d2c6eecd507314a1121d11e2e59
youngchannel <122330470+youngchannelforyou@users.noreply.github.com>
2023-09-05 02:09:38 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -49,5 +49,71 @@ function ClientHome() {
   )
 }
 
+const style = `
+  .item--Amu {
+    align-items: center;
+    background-color: #ffffff;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    padding: 14.2rem 2.8rem 14.1rem 2.7rem;
+    position: relative;
+    width: 100%;
 
-export default ClientHome;
+    .how-are-you-today-K2X {
+        color: #000000;
+        flex-shrink: 0;
+        font-family: Inter, 'Source Sans Pro';
+        font-size: 4rem;
+        font-weight: 900;
+        line-height: 1.2125;
+        margin: 0rem 4.2rem 41.4rem 0rem;
+        max-width: 29.3rem;
+    }
+
+    .group-86-9nF {
+        background-color: #fee500;
+        border-radius: 1.2rem;
+        box-sizing: border-box;
+        flex-shrink: 0;
+        height: 5rem;
+        padding: 1.5rem 11rem 1.5rem 9.0rem;
+        width: 100%;
+        max-width: 500px;
+
+        .group-88-sCT {
+            align-items: center;
+            display: flex;
+            height: 100%;
+            width: 100%;
+
+            .kakaoimg-1-DGK {
+                flex-shrink: 0;
+                height: 2rem;
+                margin-right: 1.1rem;
+                object-fit: cover;
+                vertical-align: top;
+                width: 2rem;
+            }
+
+            .item--YZV {
+                color: rgba(0, 0, 0, 0.8500000238);
+                flex-shrink: 0;
+                font-family: Inter, 'Source Sans Pro';
+                font-size: 1.5rem;
+                font-weight: 900;
+                line-height: 1.2125;
+                text-align: center;
+                white-space: nowrap;
+            }
+        }
+    }
+}`;
+
+// 컴포넌트 파일 안에 CSS 스타일 추가
+const ClientHomeStyle = () => (
+  <style jsx>{styles}</style>
+);
+
+export default {ClientHome,ClientHomeStyle};
