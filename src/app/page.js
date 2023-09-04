20e0cd0cc688b48a2734fb6ba17c1e1cdc0178cb
youngchannel <122330470+youngchannelforyou@users.noreply.github.com>
2023-09-05 00:51:37 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,13 +6,14 @@ import { useEffect, useState } from "react";
 import './login.css';
 import { GetServerSideProps } from 'next';
 
-document.cookie="safeCookie1=foo;SameSite=Lax";
-document.cookie="safeCookie2=foo";
-document.cookie="crossCookie=bar;SameSite=None;Secure";
+
 
 
 //use client
 function ClientHome() {
+  document.cookie="safeCookie1=foo;SameSite=Lax";
+  document.cookie="safeCookie2=foo";
+  document.cookie="crossCookie=bar;SameSite=None;Secure";
   // const [IsMobile, setIsMobile] = useState(false);
 
   // useEffect(() => {
