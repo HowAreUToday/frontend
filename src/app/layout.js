fe878761329d6b7f61f3f6512ca9485c330ffbf5
youngchannel <122330470+youngchannelforyou@users.noreply.github.com>
2023-10-04 11:00:07 +0900

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,8 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes" />
       </head>
       <body >
         {children}
